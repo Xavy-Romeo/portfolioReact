83fffc5c1dede637bb2c012ddfdc5aad46ed84fb
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-07-28 20:09:27 -0700

    added Switch routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Content from './pages/Content';
+import Landing from './pages/Landing';
+
 import './App.css';
 
 function App() {
   return (
-    <>
-      hello
-    </>
+    <Router>
+      <Switch>
+        
+        <Route exact path='/'>
+          <Landing />
+        </Route>
+        
+        <Route exact path='/XR'>
+          <Content />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 

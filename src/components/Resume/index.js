8234b6972cb914587b03b32c25220e4f063399f9
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-08-05 22:03:36 -0700

    temp resume placeholder

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,15 +2,26 @@ import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
 export default function Resume({ classes }) {
     return (
         <>
-            <Grid container>
-                RESUME TITLE SECTION
+            <Grid container className={classes.sectionTitleContainer} justifyContent='center'>
+                <Typography className={classes.sectionTitleShadow} variant='h1'>
+                    RESUME
+                </Typography>
+                <Typography className={classes.sectionTitle} variant='h4' component='h3'>
+                    My Resume
+                </Typography>
             </Grid>
-            <Grid container>
-                MY RESUME POSTED HERE
+
+            <Grid container className={classes.componentMainContainer} justifyContent='center'>
+                <Paper>
+                    MY RESUME POSTED HERE
+                    PENDING PENDING PENDING
+                </Paper>
+                
             </Grid>
         </>
     );
